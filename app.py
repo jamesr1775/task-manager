d4797743c7a02a41c017011ffcfbae4252a9130f
jamesr1775 <james.rooney@ucdconnect.ie>
2021-05-04 11:18:50 +0000

    fix

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from flask import Flask
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
+
+
 @app.route("/")
 
 
@@ -12,5 +15,5 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-    port=int(os.environ.get("PORT")),
-    debug=True)
+            port=int(os.environ.get("PORT")),
+            debug=True)
