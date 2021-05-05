14bf83d60e57c274021f4371c75202c3d763bdaa
jamesr1775 <james.rooney@ucdconnect.ie>
2021-05-05 02:26:09 +0000

    add log out functionality

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def register():
         mongo.db.users.insert_one(register)
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return render_template("profile.html", username=session["user"])
     return render_template("register.html")
 
 
@@ -55,6 +56,7 @@ def login():
             if check_password_hash(existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}".format(request.form.get("username")))
+                return render_template("profile.html", username=session["user"])
             else:
                 # invalid pw or username
                 flash("Incorrect Username and/or Password")
@@ -64,6 +66,24 @@ def login():
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    if session["user"]:
+        return render_template("profile.html", username=username)
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
