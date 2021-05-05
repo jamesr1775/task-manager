41624033e7573f8e2a14f53583fd3aeffd412679
jamesr1775 <james.rooney@ucdconnect.ie>
2021-05-05 11:21:04 +0000

    added form to add task

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
     $('.sidenav').sidenav({edge:"right"});
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
   });
